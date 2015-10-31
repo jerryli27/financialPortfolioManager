5ac275a5b48d18f4ef36fe000c3dd5eae7cbce6b
jerryli27 <jerrylijiaming@gmail.com>
2015-10-31 16:24:22 -0500

    attempt 3

--- test.pl
+++ test.pl
@@ -180,7 +180,9 @@ my @portfolioArray=("portfolio1","portfolio2");
 
 
 print header,
-	"<script src=\"http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js\"></script>",
+	"<link rel=\"stylesheet\" href=\"http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css\">
+ 	<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js\"></script>
+	<script src=\"http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js\"></script>",
 	$cssStyleHeader,
 	start_html('hello world'),
 	h1($usernameLink."|<a href=\"".$portfolioArray[0]."\">".$portfolioArray[0].
