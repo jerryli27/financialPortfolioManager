8a10184913c8ac78d71e4fbed64fd55285d21eab
jerryli27 <jerrylijiaming@gmail.com>
2015-10-31 13:26:38 -0500

    Added some headers from rwb

--- test.pl
+++ test.pl
@@ -1,11 +1,73 @@
 #!/usr/bin/perl -w
 
+#
+#
+# test.pl (Portfolio)
+#
+#
+# Test code for EECS 339, Northwestern University
+# 
+# Alan Fu, Jerry Li
+#
+
+#
+# The combination of -w and use strict enforces various 
+# rules that make the script more resilient and easier to run
+# as a CGI script.
+#
 use strict;
 
+# The CGI web generation stuff
+# This helps make it easy to generate active HTML content
+# from Perl
+#
+# We'll use the "standard" procedural interface to CGI
+# instead of the OO default interface
 use CGI qw(:standard);
+# This helps printing fatal errors to the browser. 
 use CGI::Carp qw(fatalsToBrowser);
+
+# The interface to the database.  The interface is essentially
+# the same no matter what the backend database is.  
+#
+# DBI is the standard database interface for Perl. Other
+# examples of such programatic interfaces are ODBC (C/C++) and JDBC (Java).
+#
+#
+# This will also load DBD::Oracle which is the driver for
+# Oracle.
+use DBI;
+
+#
+#
+# A module that makes it easy to parse relatively freeform
+# date strings into the unix epoch time (seconds since 1970)
+#
+use Time::ParseDate;
 
 print header,
 	start_html('hello world'),
 	h1('hello world'),
 	end_html();
+
+
+######################################################################
+#
+# Nothing important after this
+#
+######################################################################
+
+# The following is necessary so that DBD::Oracle can
+# find its butt
+#
+BEGIN {
+  unless ($ENV{BEGIN_BLOCK}) {
+    use Cwd;
+    $ENV{ORACLE_BASE}="/raid/oracle11g/app/oracle/product/11.2.0.1.0";
+    $ENV{ORACLE_HOME}=$ENV{ORACLE_BASE}."/db_1";
+    $ENV{ORACLE_SID}="CS339";
+    $ENV{LD_LIBRARY_PATH}=$ENV{ORACLE_HOME}."/lib";
+    $ENV{BEGIN_BLOCK} = 1;
+    exec 'env',cwd().'/'.$0,@ARGV;
+  }
+}
