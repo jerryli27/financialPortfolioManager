eb12ccad7cb7ff08c32b86a0cc8902a6f8ce3d51
jerryli27 <jerrylijiaming@gmail.com>
2015-11-07 23:34:03 -0600

    debug 1.9

--- get_stock_hist.pm
+++ get_stock_hist.pm
@@ -17,15 +17,7 @@ require Exporter;
 
 use DBI;
 # import stock_data_access because we need to exec sql
-# execsql in stock_data_access seems to give lots of error message though..
 use stock_data_access;
-
-#
-# oracle sqlplus credential
-# 
-my $dbuser="yfo776";
-my $dbpasswd="zv0XOar1o";
-
 
 # Headers copied from stock_data_access.pm
 
@@ -58,7 +50,7 @@ sub insertStockHist {
 
 
 	$q = new Finance::QuoteHist::Yahoo(%query) or die "Cannot issue query\n";
-
+	my @sqlReturn;
 	foreach $row ($q->quotes()) {
 	  # my @out;
 
@@ -66,7 +58,7 @@ sub insertStockHist {
 	  $qdate=parsedate($qdate);
 	  my $sql="INSERT INTO portfolio_stocks
 				VALUES (\'$qsymbol\', $qdate, $qopen, $qhigh, $qlow, $qclose, $qvolume)";
-	  ExecSQL($dbuser, $dbpasswd,$sql,undef);
+	  @sqlReturn=ExecStockSQL(undef,$sql);
 	}
 }
 #
@@ -75,7 +67,7 @@ sub insertStockHist {
 # You can also enter date/time.
 sub getAllStocksHist{
 	# Get a list of all the symbols of stocks, their start date, and their end date involved in our transactions.
-	my @symbols = ExecSQL($dbuser, $dbpasswd,"SELECT symbol,max(timestamp) FROM portfolio_allStocks GROUP BY symbol",undef);
+	my @symbols = ExecStockSQL(undef,"SELECT symbol,max(timestamp) FROM portfolio_allStocks GROUP BY symbol");
 	# Get current time
 	my $to = parsedate($to);
 	$to = ParseDateString("epoch $to");
@@ -92,7 +84,7 @@ sub getAllStocksHist{
 sub insertLatestStockHist{
 	@info=("date","time","high","low","close","open","volume","last");
 	# Get a list of all the symbols of stocks, their start date, and their end date involved in our transactions.
-	my @symbols = ExecSQL($dbuser, $dbpasswd,"SELECT DISTINCT symbol FROM portfolio_allStocks",undef);
+	my @symbols = ExecStockSQL(undef,"SELECT DISTINCT symbol FROM portfolio_allStocks");
 
 	$con=Finance::Quote->new();
 
@@ -108,9 +100,9 @@ sub insertLatestStockHist{
 			if (defined($quotes{$symbol,"date"})&&defined($quotes{$symbol,"time"})) {
 				my $time=parsedate($quotes{$symbol,"date"}." ".$quotes{$symbol,"time"});
 				$time = ParseDateString("epoch $time");
-	  			ExecSQL($dbuser, $dbpasswd,"INSERT INTO portfolio_stocks
+	  			ExecStockSQL(undef,"INSERT INTO portfolio_stocks
 				VALUES ($symbol, $time, $$quotes{$symbol,\"open\"}, $$quotes{$symbol,\"high\"}, 
-				$$quotes{$symbol,\"low\"}, $$quotes{$symbol,\"close\"}, $$quotes{$symbol,\"volume\"});",undef);
+				$$quotes{$symbol,\"low\"}, $$quotes{$symbol,\"close\"}, $$quotes{$symbol,\"volume\"});");
 			}
 			# foreach $key (@info) {
 			#     if (defined($quotes{$symbol,$key})) {
@@ -122,177 +114,3 @@ sub insertLatestStockHist{
 	}
 }
 
-
-#
-# Given a list of scalars, or a list of references to lists, generates
-# an html table
-#
-#
-# $type = undef || 2D => @list is list of references to row lists
-# $type = ROW   => @list is a row
-# $type = COL   => @list is a column
-#
-# $headerlistref points to a list of header columns
-#
-#
-# $html = MakeTable($id, $type, $headerlistref,@list);
-#
-sub MakeTable {
-  my ($id,$type,$headerlistref,@list)=@_;
-  my $out;
-  #
-  # Check to see if there is anything to output
-  #
-  if ((defined $headerlistref) || ($#list>=0)) {
-    # if there is, begin a table
-    #
-    $out="<table id=\"$id\" border>";
-    #
-    # if there is a header list, then output it in bold
-    #
-    if (defined $headerlistref) { 
-      $out.="<tr>".join("",(map {"<td><b>$_</b></td>"} @{$headerlistref}))."</tr>";
-    }
-    #
-    # If it's a single row, just output it in an obvious way
-    #
-    if ($type eq "ROW") { 
-      #
-      # map {code} @list means "apply this code to every member of the list
-      # and return the modified list.  $_ is the current list member
-      #
-      $out.="<tr>".(map {defined($_) ? "<td>$_</td>" : "<td>(null)</td>" } @list)."</tr>";
-    } elsif ($type eq "COL") { 
-      #
-      # ditto for a single column
-      #
-      $out.=join("",map {defined($_) ? "<tr><td>$_</td></tr>" : "<tr><td>(null)</td></tr>"} @list);
-    } else { 
-      #
-      # For a 2D table, it's a bit more complicated...
-      #
-      $out.= join("",map {"<tr>$_</tr>"} (map {join("",map {defined($_) ? "<td>$_</td>" : "<td>(null)</td>"} @{$_})} @list));
-    }
-    $out.="</table>";
-  } else {
-    # if no header row or list, then just say none.
-    $out.="(none)";
-  }
-  return $out;
-}
-
-
-#
-# Given a list of scalars, or a list of references to lists, generates
-# an HTML <pre> section, one line per row, columns are tab-deliminted
-#
-#
-# $type = undef || 2D => @list is list of references to row lists
-# $type = ROW   => @list is a row
-# $type = COL   => @list is a column
-#
-#
-# $html = MakeRaw($id, $type, @list);
-#
-sub MakeRaw {
-  my ($id, $type,@list)=@_;
-  my $out;
-  #
-  # Check to see if there is anything to output
-  #
-  $out="<pre id=\"$id\">\n";
-  #
-  # If it's a single row, just output it in an obvious way
-  #
-  if ($type eq "ROW") { 
-    #
-    #
-    $out.=join("\n",map { defined($_) ? $_ : "(null)" } @list);
-    $out.="\n";
-  } else {
-    #
-    # For a 2D table
-    #
-    foreach my $r (@list) { 
-      $out.= join("\t", map { defined($_) ? $_ : "(null)" } @{$r});
-      $out.="\n";
-    }
-  }
-  $out.="</pre>\n";
-  return $out;
-}
-# @list=ExecSQL($user, $password, $querystring, $type, @fill);
-#
-# Executes a SQL statement.  If $type is "ROW", returns first row in list
-# if $type is "COL" returns first column.  Otherwise, returns
-# the whole result table as a list of references to row lists.
-# @fill are the fillers for positional parameters in $querystring
-#
-# ExecSQL executes "die" on failure.
-#
-sub ExecSQL {
-  my ($user, $passwd, $querystring, $type, @fill) =@_;
-  if ($debug) { 
-    # if we are recording inputs, just push the query string and fill list onto the 
-    # global sqlinput list
-    push @sqlinput, "$querystring (".join(",",map {"'$_'"} @fill).")";
-  }
-  my $dbh = DBI->connect("DBI:Oracle:",$user,$passwd);
-  if (not $dbh) { 
-    # if the connect failed, record the reason to the sqloutput list (if set)
-    # and then die.
-    if ($debug) { 
-      push @sqloutput, "<b>ERROR: Can't connect to the database because of ".$DBI::errstr."</b>";
-    }
-    die "Can't connect to database because of ".$DBI::errstr;
-  }
-  my $sth = $dbh->prepare($querystring);
-  if (not $sth) { 
-    #
-    # If prepare failed, then record reason to sqloutput and then die
-    #
-    if ($debug) { 
-      push @sqloutput, "<b>ERROR: Can't prepare '$querystring' because of ".$DBI::errstr."</b>";
-    }
-    my $errstr="Can't prepare $querystring because of ".$DBI::errstr;
-    $dbh->disconnect();
-    die $errstr;
-  }
-  if (not $sth->execute(@fill)) { 
-    #
-    # if exec failed, record to sqlout and die.
-    if ($debug) { 
-      push @sqloutput, "<b>ERROR: Can't execute '$querystring' with fill (".join(",",map {"'$_'"} @fill).") because of ".$DBI::errstr."</b>";
-    }
-    my $errstr="Can't execute $querystring with fill (".join(",",map {"'$_'"} @fill).") because of ".$DBI::errstr;
-    $dbh->disconnect();
-    die $errstr;
-  }
-  #
-  # The rest assumes that the data will be forthcoming.
-  #
-  #
-  my @data;
-  if (defined $type and $type eq "ROW") { 
-    @data=$sth->fetchrow_array();
-    $sth->finish();
-    if ($debug) {push @sqloutput, MakeTable("debug_sqloutput","ROW",undef,@data);}
-    $dbh->disconnect();
-    return @data;
-  }
-  my @ret;
-  while (@data=$sth->fetchrow_array()) {
-    push @ret, [@data];
-  }
-  if (defined $type and $type eq "COL") { 
-    @data = map {$_->[0]} @ret;
-    $sth->finish();
-    if ($debug) {push @sqloutput, MakeTable("debug_sqloutput","COL",undef,@data);}
-    $dbh->disconnect();
-    return @data;
-  }
-  $sth->finish();
-  if ($debug) {push @sqloutput, MakeTable("debug_sql_output","2D",undef,@ret);}
-  $dbh->disconnect();
-  return @ret;
-}
