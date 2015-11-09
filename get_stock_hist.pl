4f24ee4393fa94c3ed5e47077169978b456b8a1c
jerryli27 <jerrylijiaming@gmail.com>
2015-11-09 17:51:07 -0600

    ..

--- get_stock_hist.pl
+++ get_stock_hist.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 
 use get_stock_hist;
-insertStockHistUnixTime("adbe",1036885762,1446966324);
+insertStockHistUnixTime("^GSPC",1036885762,1446966324);
 #getAllStocksHist();
 #insertLatestStockHist(); To be tested later
