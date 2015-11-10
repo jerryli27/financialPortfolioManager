05d309291c0d439b6585dd738ab7c32e201bd072
jerryli27 <jerrylijiaming@gmail.com>
2015-11-09 23:13:35 -0600

    ..conflicts

--- autoStockUpdates.pl
+++ autoStockUpdates.pl
@@ -1,8 +1,4 @@
 #!/usr/bin/perl -w
 
 use get_stock_hist;
-<<<<<<< HEAD
 insertLatestStockHist();
-=======
-insertLatestStockHist(); To be tested later
->>>>>>> parent of d57cf1a... Revert "added a perl file for crontab"
