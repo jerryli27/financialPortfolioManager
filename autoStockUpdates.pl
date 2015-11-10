758ef861b74bc90b2a5d67de5c4111c8e88525e5
jerryli27 <jerrylijiaming@gmail.com>
2015-11-09 23:13:04 -0600

    Revert "Revert "added a perl file for crontab""
    This reverts commit d57cf1acd2af17b5f9ce97db213dba7ca4e31a11.
    
    Conflicts:
    	autoStockUpdates.pl

--- autoStockUpdates.pl
+++ autoStockUpdates.pl
@@ -1,4 +1,8 @@
 #!/usr/bin/perl -w
 
 use get_stock_hist;
+<<<<<<< HEAD
 insertLatestStockHist();
+=======
+insertLatestStockHist(); To be tested later
+>>>>>>> parent of d57cf1a... Revert "added a perl file for crontab"
