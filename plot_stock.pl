4ad91ec6827d57c437a1c2f74ef929554b90dbd1
jerryli27 <jerrylijiaming@gmail.com>
2015-11-10 00:28:56 -0600

    debug 1.5

--- plot_stock.pl
+++ plot_stock.pl
@@ -63,14 +63,19 @@ if (!defined($type) || $type eq "text" || !($type eq "plot") ) {
 
 
 #my @rows = ExecStockSQL("2D","select timestamp, close from portfolio_allStocks where symbol=? order by timestamp",$symbol);
-my @rows = `$dir/time_series_symbol_project.pl $symbol 4 AWAIT 200 AR 16`;
-print("output:");
-print(@rows);
+my $output = `$dir/time_series_symbol_project.pl $symbol 4 AWAIT 200 AR 16`;
+my @rows=split /\s+/, $output ;
+# print(@rows);
+my $counter=0;
 
 if ($type eq "text") { 
   print "<pre>";
   foreach my $r (@rows) {
-    print $r->[0], "\t", $r->[1], "\n";
+    if ($counter%2==0)
+      print $r, "\t"
+    else
+      print $r, "\n";
+    $counter++;
   }
   print "</pre>";
 
@@ -90,11 +95,11 @@ if ($type eq "text") {
   print GNUPLOT "set output\n";             # output the PNG to stdout
   print GNUPLOT "plot '-' using 1:2 with linespoints\n"; # feed it data to plot
   foreach my $r (@rows) {
-    if ($r->[1]==0){
-        print GNUPLOT $r->[0], "\t", $r->[2], "\n";
-      }else{
-        print GNUPLOT $r->[0], "\t", $r->[1], "\n";
-      }
+    if ($counter%2==0)
+      print GNUPLOT $r, "\t"
+    else
+      print GNUPLOT $r, "\n";
+    $counter++;
   }
   print GNUPLOT "e\n"; # end of data
 
