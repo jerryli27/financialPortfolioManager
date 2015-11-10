7224821b86f11d0ef16798f6337cdb0b4a5cbab9
jerryli27 <jerrylijiaming@gmail.com>
2015-11-09 18:49:51 -0600

    debug 2.8

--- get_stock_hist.pm
+++ get_stock_hist.pm
@@ -203,7 +203,7 @@ sub insertLatestStockHist{
 				eval {
 					my $time=parsedate($quotes{$symbol,"date"}." ".$quotes{$symbol,"time"}); #epoch time
 					$sql="INSERT INTO portfolio_stocks
-					VALUES(\"$symbol\", $time, $quotes{$symbol,\"open\"}, $quotes{$symbol,\"high\"}, 
+					VALUES (\'$symbol\', $time, $quotes{$symbol,\"open\"}, $quotes{$symbol,\"high\"}, 
 					$quotes{$symbol,\"low\"}, $quotes{$symbol,\"close\"}, $quotes{$symbol,\"volume\"})";
 					#send the query
 				 	$sth = $dbh->prepare($sql);
