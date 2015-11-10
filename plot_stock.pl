62a13b4e023f4d9ce888fa456b2fa75c4618e108
jerryli27 <jerrylijiaming@gmail.com>
2015-11-10 00:57:34 -0600

    debug 1.8.2

--- plot_stock.pl
+++ plot_stock.pl
@@ -99,8 +99,6 @@ if ($type eq "text") {
   
   print GNUPLOT "set term png\n";           # we want it to produce a PNG
   print GNUPLOT "set output\n";             # output the PNG to stdout
-  print GNUPLOT "set xlabel \"Day Number\"";
-  print GNUPLOT "set ylabel \"Price\"";
   print GNUPLOT "plot '-' using 1:2 with linespoints\n"; # feed it data to plot
   foreach my $r (@rows) {
     if ($r==0 && $counter!=0){
@@ -116,6 +114,8 @@ if ($type eq "text") {
     }
   }
   print GNUPLOT "e\n"; # end of data
+  print GNUPLOT "set xlabel \"Day Number\"";
+  print GNUPLOT "set ylabel \"Price\"";
   #
   # Here gnuplot will print the image content
   #
