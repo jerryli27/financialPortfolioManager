807c97e8d24e8b7024e20f251184b80d03a2a886
jerryli27 <jerrylijiaming@gmail.com>
2015-11-11 15:40:08 -0600

    minor change

--- test.pl
+++ test.pl
@@ -601,7 +601,7 @@ if ($action eq "detail") {
 
 
 print "All source code are in <a href=\"http://murphy.wot.eecs.northwestern.edu/~jlt709/portfolio\"> http://murphy.wot.eecs.northwestern.edu/~jlt709/portfolio</a> folder<br>
-For test purposes, you can use the user: testuser password: testuser
+For test purposes, you can use the user: testuser password: testuser<br>
 Click <a href=\"http://murphy.wot.eecs.northwestern.edu/~jlt709/portfolio/Project 2 mockup.pdf\"> here</a> for the project mockup <br>
 Click <a href=\"http://murphy.wot.eecs.northwestern.edu/~jlt709/portfolio/ER Diagram.pdf\"> here</a> for the ER diagram <br>
 Click <a href=\"http://murphy.wot.eecs.northwestern.edu/~jlt709/portfolio/Relational design.txt\"> here</a> for the relational design <br>";
